15cf59e280af0c92b068eea3fdf29b895d0c3be7
Denis <dev@webernetic.by>
2022-05-11 14:21:03 +0300

    configs

---
+++ config-example.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://lovelace.by/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://lovelace.by/');
+
+// DIR
+define('DIR_APPLICATION', 'DIR_APPLICATION');
+define('DIR_SYSTEM', 'DIR_SYSTEM');
+define('DIR_IMAGE', 'DIR_IMAGE');
+define('DIR_STORAGE', 'DIR_STORAGE');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'DIR_LANGUAGE/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'DIR_TEMPLATE/');
+define('DIR_CONFIG', DIR_SYSTEM . 'DIR_CONFIG/');
+define('DIR_CACHE', DIR_STORAGE . 'DIR_CACHE/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'DIR_DOWNLOAD/');
+define('DIR_LOGS', DIR_STORAGE . 'DIR_LOGS/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'DIR_MODIFICATION/');
+define('DIR_SESSION', DIR_STORAGE . 'DIR_SESSION/');
+define('DIR_UPLOAD', DIR_STORAGE . 'DIR_UPLOAD/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'USERNAME');
+define('DB_PASSWORD', 'PASSWORD');
+define('DB_DATABASE', 'DB_NAME');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'll_');
+define('APP_MOD', 'production');
+define('BEPAY_LOGIN', '17260');
+define('BEPAY_PASSWORD', 'e261ce7ce2c3dbb61f95f95c18942758925a7990ff8fc98dd3c58fe1ff3c9903');

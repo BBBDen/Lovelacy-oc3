acbb8cc595a9cc7a94251d4a2e278c0577fa3e2f
den4ikos <denasius89@yandex.ru>
2022-06-10 11:08:33 +0300

    fixes from server and retail

--- system/library/retailcrm/lib/service/RetailcrmCustomerConverter.php
+++ system/library/retailcrm/lib/service/RetailcrmCustomerConverter.php
@@ -27,6 +27,20 @@ class RetailcrmCustomerConverter {
         return $this;
     }
 
+    public function materialSelect($material)
+    {
+        if ($material == 'chlopock') {
+            return $material = 'Хлопок';
+        } else if ($material == 'setka') {
+            return $material = 'Сетка';
+        } else if ($material == 'cruzhevo') {
+            return $material = 'Кружево';
+        } else if ($material == 'all') {
+            return $material = 'Все';
+        }
+
+    }
+    
     public function setCustomerData() {
         $this->data['externalId'] = $this->customer_data['customer_id'];
         $this->data['firstName'] = $this->customer_data['firstname'];
@@ -41,9 +55,96 @@ class RetailcrmCustomerConverter {
                 )
             );
         }
+        $subscribe = false;
+        if ($this->customer_data['subscribe']['pay_status'] == 'successful')
+        {
+            $this->customer_data['subscribe']['pay_status'] = 'true';
+        }
+        else
+        {
+            $this->customer_data['subscribe']['pay_status'] = 'false';
+        }
+        if (preg_match_all('~"([^"]*)"~u' , $this->customer_data['subscribe']['user_data'] , $m)) {
+
+            if (count($m[1]) == 22 )
+            {
+                $this->data['customFields'] = array (
+                    'bust' => $m[1][1],
+                    'girth_under_bust2' =>$m[1][3],
+                    'hip_girth' => $m[1][5],
+                    'cup_size' =>$m[1][7],
+                    'wear_chokers' => $m[1][11],
+                    'wear_straps' => $m[1][13],
+                    'material' => $this->materialSelect($m[1][15]),
+                    'prefer_of_color' => $m[1][17],
+                    'put_in_bag' => $m[1][19],
+                    'wishes_didnt_ask_about' => $m[1][21],
+                    'instagram_account' => $this->customer_data['social_nick'],
+                    'subscribe' => $this->customer_data['subscribe']['pay_status']
+                );
+            }
+            else if(count($m[1]) == 23)
+            {
+                $this->data['customFields'] = array (
+                    'bust' => $m[1][1],
+                    'girth_under_bust2' =>$m[1][3],
+                    'hip_girth' => $m[1][5],
+                    'cup_size' =>$m[1][7],
+                    'wear_chokers' => $m[1][11],
+                    'wear_straps' => $m[1][13],
+                    'material' =>$this->materialSelect($m[1][15]).','.$this->materialSelect($m[1][16]),
+                    'prefer_of_color' => $m[1][18],
+                    'put_in_bag' => $m[1][20],
+                    'wishes_didnt_ask_about' => $m[1][22],
+                    'instagram_account' => $this->customer_data['social_nick'],
+                    'subscribe' => $this->customer_data['subscribe']['pay_status']
+                );
+            }
+            else if(count($m[1]) == 24)
+            {
+                $this->data['customFields'] = array (
+                    'bust' => $m[1][1],
+                    'girth_under_bust2' =>$m[1][3],
+                    'hip_girth' => $m[1][5],
+                    'cup_size' =>$m[1][7],
+                    'wear_chokers' => $m[1][11],
+                    'wear_straps' => $m[1][13],
+                    'material' => $this->materialSelect($m[1][15]).','.$this->materialSelect($m[1][16]).','.$this->materialSelect($m[1][17]),
+                    'prefer_of_color' => $m[1][19],
+                    'put_in_bag' => $m[1][21],
+                    'wishes_didnt_ask_about' => $m[1][23],
+                    'instagram_account' => $this->customer_data['social_nick'],
+                    'subscribe' => $this->customer_data['subscribe']['pay_status']
+                );
+            }
+            else if(count($m[1]) == 25)
+            {
+                $this->data['customFields'] = array (
+                    'bust' => $m[1][1],
+                    'girth_under_bust2' =>$m[1][3],
+                    'hip_girth' => $m[1][5],
+                    'cup_size' =>$m[1][7],
+                    'wear_chokers' => $m[1][11],
+                    'wear_straps' => $m[1][13],
+                    'material' => $this->materialSelect($m[1][15]).','.$this->materialSelect($m[1][16]).','.$this->materialSelect($m[1][17]).','.$this->materialSelect($m[1][18]),
+                    'prefer_of_color' => $m[1][20],
+                    'put_in_bag' => $m[1][22],
+                    'wishes_didnt_ask_about' => $m[1][24],
+                    'instagram_account' => $this->customer_data['social_nick'],
+                    'subscribe' => $this->customer_data['subscribe']['pay_status']
+                );
+            }
+
+
+        }else{
+            $this->data['customFields'] = array (
+                'instagram_account' => $this->customer_data['social_nick']
+            );
+        }
 
         return $this;
     }
+
 
     public function setAddress() {
         if (!empty($this->address)) {
